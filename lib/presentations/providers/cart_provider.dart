7f3676eac300b0e3e51035872e924e79a654ef65
B052291 <B052291@servexternos.santandertecnologia.com.ar>
2024-05-08 22:34:10 -0300

    Provider carrito 8/5/2024

--- lib/presentations/providers/cart_provider.dart
+++ lib/presentations/providers/cart_provider.dart
@@ -3,6 +3,34 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:proyecto_final_grupo_6/core/carrito.dart';
 import 'package:proyecto_final_grupo_6/presentations/entities/cartera.dart';
 
-final Provider<List<Cartera>> carritoProvider = Provider((ref) => carrito);
+final StateNotifierProvider<CarritoStateNotifier, List<Cartera>> carritoProvider = StateNotifierProvider<CarritoStateNotifier, List<Cartera>>((ref) => CarritoStateNotifier(carrito));
+
+class CarritoStateNotifier extends StateNotifier<List<Cartera>> {
+  CarritoStateNotifier(state) : super(state ?? []);
+  
+  void eliminarProducto(String titulo) {
+    state = state.where((p) => p.titulo != titulo).toList();
+  }
+
+  void sumarCantidadProducto(Cartera producto) {
+    List<Cartera> carritoAux = [...state];
+    for (var e in carritoAux) {
+      if(e.titulo == producto.titulo){
+        e.cantidad++;
+      }
+    }
+    state = carritoAux;
+  }
+
+  void restarCantidadProducto(Cartera producto) {
+    List<Cartera> carritoAux = [...state];
+    for (var e in carritoAux) {
+      if(e.titulo == producto.titulo && e.cantidad > 1){
+        e.cantidad--;
+      }
+    }
+    state = carritoAux;
+  }
+}
 
 StateProvider<double> subTotalProvider = StateProvider<double>((ref) => subTotal);
